5edecf59528126b3e3a5fb700036ddad50cb3e7f
Stephen Lee <stephenl7797@gmail.com>
2016-05-06 16:07:19 -0400

    add index.html to wt

---
+++ res/_site/js/all.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('#index-outer').css('background-position-y',-(465+scrolled*1.25)+'px'); // here you scroll downward
+}
