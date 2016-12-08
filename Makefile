ce0a887b2878ecbece6f10fd1d9ff13daa705fe2
Stephen Lee <stephenl7797@gmail.com>
2016-12-08 00:52:48 -0500

    disqus integration & new bg

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 
 all:
 	cd res; \
-	stack runghc ../site.hs build; \
-	cp _site/* ../ -r \
+	stack exec site build; \
+	cp _site/* ../ -rf; \
+	cd ..; \
+	rm -rf ./_cache
 
 clean:
 	rm -rf index.html posts about.html contact.markdown _site _cache archive.html images css js res/_site res/_cache
--- res/js/all.js
+++ res/js/all.js
@@ -4,5 +4,6 @@ $(window).scroll(function(e){
 
 function parallax(){
   var scrolled = $(window).scrollTop();
-  $('#index-outer').css('background-position-y',-(465+scrolled*1.25)+'px'); // here you scroll downward
+  //$('#index-outer').css('background-position-y',-(465+scrolled*1.25)+'px'); // here you scroll downward
+  $('#index-outer').css('background-position-y',-(scrolled*0.3)+'px'); // here you scroll downward
 }
