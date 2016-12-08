9da5bc2b1e7e9e9da062c832d99c07b3d9a1785b
Stephen Lee <stephenl7797@gmail.com>
2016-12-08 11:08:02 -0500

    iz

--- js/all.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(e){
-  parallax();
-});
-
-function parallax(){
-  var scrolled = $(window).scrollTop();
-  //$('#index-outer').css('background-position-y',-(465+scrolled*1.25)+'px'); // here you scroll downward
-  $('#index-outer').css('background-position-y',-(scrolled*0.3)+'px'); // here you scroll downward
-}
