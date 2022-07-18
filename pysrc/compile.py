883c14dff9f7b681655af31cb8e9720d628a6a9a
Stephen Lee <stephenl7797@gmail.com>
2022-07-18 12:16:40 -0400

    new blog

--- pysrc/compile.py
+++ pysrc/compile.py
@@ -16,6 +16,18 @@ def getHeaderClass(l, *a,**k):
     l = max(min(int(l),6),1)
     return getattr(D,'h'+str(l))
 
+def getMeta(lines):
+    if lines[0] == '```':
+        meta = {}
+        while i < len(lines):
+            i += 1
+            sline = lines[i].strip()
+            if sline == '```': break
+            k,v = (s.strip() for s in sline.split(':'))
+            meta[k] = v
+        return meta, lines[i:]
+    else: return {}, lines
+
 def processLine(line):
     # Handle image
 
@@ -49,10 +61,16 @@ def parseMarkdownToHtml(title, lines):
     i = 0
     doc = dominate.document(title=title)
     with doc.head:
-        D.link(rel='stylesheet', href='res/main.css')
-        D.link(rel='stylesheet', href='res/boostrap.darkly.css')
-        D.link(rel='stylesheet', href='res/pygments.css')
+        D.link(rel='stylesheet', href='../res/main.css')
+        D.link(rel='stylesheet', href='../res/boostrap.darkly.css')
+        D.link(rel='stylesheet', href='../res/pygments.css')
         #script(type='text/javascript', src='script.js')
+        D.script(type='text/javascript', src='https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js')
+        roboto_weight = 400
+        doc.head.add(dominate.util.raw('''
+<link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jura&family=Roboto:wght@{}&display=swap" rel="stylesheet">
+'''.format(roboto_weight)))
 
     with doc.body as body:
         body.add(dominate.util.raw(get_navbar()))
@@ -62,7 +80,7 @@ def parseMarkdownToHtml(title, lines):
 
             with D.div(_class='page-header'):
                 with D.div(_class='col-lg-8 col-md-7 col-sm-6'):
-                    pass
+                    D.h1(title)
 
             while i < len(lines):
                 line = lines[i]
@@ -166,53 +184,101 @@ def get_navbar():
     <div class="collapse navbar-collapse" id="navbarColor02">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Home
+          <a class="nav-link active" href="./index.html">Blog
             <span class="visually-hidden">(current)</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="https://github.com/steplee">GitHub</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
+
+        <!-- This is not working -->
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" id="dropdownMenuButton1" data-toggle="dropdown1">Links</a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton1" id="dropdown1">
+            <a class="dropdown-item" href="https://www.shadertoy.com/user/stephenl7797">ShaderToy</a>
+            <a class="dropdown-item" href="https://www.shadertoy.com/user/stephenl7797">ShaderToy</a>
           </div>
         </li>
+
       </ul>
     </div>
   </div>
 </nav>
     '''
 
-def main():
-    makePygmentsCss()
 
+def iterPosts():
     dir = 'posts'
     for file in os.listdir(dir):
         path = os.path.join(dir,file)
-        if os.path.isdir(path):
-            pass
-        else:
-            day,month,year,title = file.split('_', 3)
+        if not os.path.isdir(path):
+            day,month,year,title0 = file.split('_', 3)
 
-            title = title.replace('.md','')
-            title = ' '.join(s.capitalize() for s in title.split('_'))
+            title0 = title0.replace('.md','')
+            title0 = ' '.join(s.capitalize() for s in title0.split('_'))
 
             with open(path,'r') as fp: lines = fp.readlines()
-            src = parseMarkdownToHtml(title, lines)
-            with open('out.html', 'w') as fp: print(src,file=fp)
+            meta, lines = getMeta(lines)
+            meta['title'] = meta.get('title', title0) # if not title given, use from filename
+
+            yield file, meta, lines
+
+
+def makePosts():
+    makePygmentsCss()
+    try:
+        os.makedirs('html')
+    except: pass
+
+    posts = []
+
+    for file, meta, lines in iterPosts():
+        title = meta['title']
+        src = parseMarkdownToHtml(title, lines)
+        outFileName = 'html/{}.html'.format(file.rsplit('/',1)[-1].rsplit('.')[0])
+        with open(outFileName, 'w') as fp: print(src,file=fp)
+        posts.append((title,outFileName))
+
+    return posts
+
+
+
+def makeIndex(postFileNames):
+
+    doc = dominate.document(title='Steplee')
+    with doc.head:
+        D.link(rel='stylesheet', href='res/main.css')
+        D.link(rel='stylesheet', href='res/boostrap.darkly.css')
+        D.link(rel='stylesheet', href='res/pygments.css')
+        #script(type='text/javascript', src='script.js')
+        D.script(type='text/javascript', src='https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js')
+        roboto_weight = 400
+        doc.head.add(dominate.util.raw('''
+<link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jura&family=Roboto:wght@{}&display=swap" rel="stylesheet">
+'''.format(roboto_weight)))
+
+    with doc.body as body:
+        body.add(dominate.util.raw(get_navbar()))
+        body.add(dominate.util.raw(get_mainImage()))
+
+        with D.div(_class='container') as container:
+
+            with D.div(_class='page-header'):
+                with D.div(_class='col-lg-8 col-md-7 col-sm-6'):
+                    pass
+
+            with D.div(_class='postList'):
+                D.h2('Posts')
+                for title,link in posts:
+                    with D.ul():
+                        D.li(D.a(title, href=link, _class='postLink'))
+
+    src = doc.render()
+    with open('index.html', 'w') as fp: print(src,file=fp)
 
 
 if __name__ == '__main__':
-    main()
+    posts = makePosts()
+    makeIndex(posts)
