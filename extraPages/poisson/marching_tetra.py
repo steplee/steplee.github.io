3e18713c678e38d3ea4ef06fc3b012b5a2b2e28a
Stephen Lee <stephenl7797@gmail.com>
2023-04-10 11:25:06 -0400

    Fix winding order issue (tedious) and add compute_normals()

--- extraPages/poisson/marching_tetra.py
+++ extraPages/poisson/marching_tetra.py
@@ -84,26 +84,59 @@ def test_replicate():
     sys.exit(0)
 # test_replicate()
 
+def compute_normal_batch(p):
+    assert p.ndim == 3 and p.size(1) == 3 and p.size(2) == 3
+    # Subtract a from b and c, then cross those results
+    aff = p[:, 1:, :] - p[:, 0:1, :]
+    return torch.cross(aff[:, 0], aff[:, 1])
+
+def compute_normals(positions, triInds):
+    triInds = triInds.view(-1,3).long()
+    NV = positions.size(0)
+    NT = triInds.size(0)
+
+    nrmls = torch.zeros_like(positions)
+
+    # [NT, 3, 3]
+    triPts = positions[triInds]
+    # [NT, 3]
+    crossed = compute_normal_batch(triPts)
+
+    # NOTE: We *DO NOT* normalize the raw cross products.
+    #       This makes weight to final normal proportional to face size.
+    #
+    # We have to go from [NT,3] back to [NV,3] entries
+    coo = triInds.view(1,-1)
+    val = crossed.view(NT, 1, 3).repeat(1,3,1).view(NT*3,3)
+    print(f' - sparse tensor sizes: {coo.shape} {val.shape}')
+
+    nrls = torch.sparse_coo_tensor(coo, val).coalesce().values()
+    nrls = nrls.div_(nrls.norm(dim=1,keepdim=True))
+    return nrls
+
+
+
+
 
 # Returns up to two tris per input.
 # Should be called 6 times per grid cell
-def marching_tetra_tri(positions, vals, scale,  iso, v0,v1,v2,v3):
+def marching_tetra_tri(positions, vals, scale,  iso, v0,v1,v2,v3, swap):
     N = positions.size(0)
     assert positions.size(1) == 3
     assert vals.size() == (N,8)
     D = positions.device
 
-    # Must be long -- cannot be byte. That gets interpreted as mask.
+    # Must be long -- cannot be byte. That would be interpreted as a mask later on.
     triIndex  = torch.zeros((N), dtype=torch.int64, device=D)
     triIndex |= (1) * (vals[:,v0] < iso).long()
     triIndex |= (2) * (vals[:,v1] < iso).long()
     triIndex |= (4) * (vals[:,v2] < iso).long()
     triIndex |= (8) * (vals[:,v3] < iso).long()
-    print(' - triIndex shape',triIndex.shape)
-    # print(triIndex)
+    # print(' - triIndex shape',triIndex.shape)
 
     # This replaces the switch statement in `PolygoniseTri`
     # This is more like Borke's marching cubes impl.
+    # It will allow us to implement the procedure in batch fashion with pytorch.
     table1 = {}
     # table1[0x0] = table1[0xF] = None
     table1[0x1] = table1[0xE] = (v0,v1, v0,v2, v0,v3)
@@ -129,10 +162,25 @@ def marching_tetra_tri(positions, vals, scale,  iso, v0,v1,v2,v3):
     #      A triangle has 3 vertices
     #      A vertex is blended from two grid vertices
     table = torch.zeros((16,2,3,2), device=D, dtype=torch.int64) - 1
-    for k,v in table1.items(): table[k, 0, :, :] = torch.LongTensor(v).to(D).view(3, 2)
-    for k,v in table2.items(): table[k, 1, :, :] = torch.LongTensor(v).to(D).view(3, 2)
-    # print('table\n',table)
 
+    #
+    # NOTE: I had to fix winding order of triangles. It was tedious.
+    # It was necessary to selectively swap some groups-of-4-corners, hence the `swap` argument.
+    # I had to go through each 16 and each of the 6 groups-of-4. Luckily it factored out given the swap argument.
+    # NOTE: This could be avoided by re-arranging the table and groups-of-4.
+    #
+    if 0:
+        for k,v in table1.items(): table[k, 0, :, :] = torch.LongTensor(v).to(D).view(3, 2)
+        for k,v in table2.items(): table[k, 1, :, :] = torch.LongTensor(v).to(D).view(3, 2)
+    else:
+        for k,v in table1.items():
+            if swap ^ (k in (0x3,0x7,0x9,0xA,0xB,0xD,0xE)): table[k, 0, :, :] = torch.LongTensor(v).to(D).view(3, 2)
+            else:        table[k, 0, :, :] = torch.LongTensor(v).to(D).view(3, 2)[[1,0,2]]
+        for k,v in table2.items():
+            if swap ^ (k in (0x3,0x5,0x9)): table[k, 1, :, :] = torch.LongTensor(v).to(D).view(3, 2)[[1,0,2]]
+            else:        table[k, 1, :, :] = torch.LongTensor(v).to(D).view(3, 2)
+    # print('table\n',table)
+    # for k in range(16): # if k != 0xC: table[k] = -1 table[k,0]=-1
 
     # Up to 2 tris each input. Each tri has 3 points. Each point has 3 coords.
     out = torch.zeros((N,2,3,3), device=D)
@@ -148,9 +196,9 @@ def marching_tetra_tri(positions, vals, scale,  iso, v0,v1,v2,v3):
         bp = p[bi]
 
         mu = (iso - av) / (bv-av)
-        mu.masked_fill_((iso-av).abs() < 1e-5, 0)
-        mu.masked_fill_((iso-bv).abs() < 1e-5, 1)
-        mu.masked_fill_((av-bv).abs() < 1e-5, 0)
+        mu.masked_fill_((iso-av).abs() < 1e-8, 0)
+        mu.masked_fill_((iso-bv).abs() < 1e-8, 1)
+        mu.masked_fill_((av-bv).abs() < 1e-8, 0)
 
         op = positions + ap + mu.view(-1,1)*(bp-ap)
         op[~valid_mask] = -1
@@ -160,8 +208,7 @@ def marching_tetra_tri(positions, vals, scale,  iso, v0,v1,v2,v3):
         for vertexId in range(3):
             out[:,triId,vertexId,:] = blend(table[triIndex, triId, vertexId, 0], table[triIndex, triId, vertexId, 1])
 
-    # FIXME: Fix winding orders. I guess you have to look at isovalue and flip accordingly?
-
+    # NOTE: The output consists of plenty of <-1,-1,-1> points which should be removed.
     return out
 
 # http://paulbourke.net/geometry/polygonise/source1.c
@@ -170,7 +217,7 @@ def marching_tetra_tri(positions, vals, scale,  iso, v0,v1,v2,v3):
 #
 # `positions` should be a floating point tensor [N,3] of 3d left-back-top points.
 # `vals` should be a floating point tensor [N,8] of isovalues of each of the points and it's seven neighbours.
-# The order should match that of `replicate_to_gridcells()`, a truth table in ZYX order.
+# The order should match that of `replicate_to_gridcells()`: a truth table in ZYX order.
 #
 def marching_tetra(positions, vals, iso=0, gridScale=1):
     N,D = positions.size(0), positions.device
@@ -183,31 +230,30 @@ def marching_tetra(positions, vals, iso=0, gridScale=1):
     # vals.div_(scale)
 
     groups = (
-        (0,2,3,7),
-        (0,2,6,7),
-        (0,4,6,7),
-        (0,6,1,2),
-        (0,6,1,4),
-        (5,6,1,4),
+        (False, (0,2,3,7)),
+        (True , (0,2,6,7)),
+        (False, (0,4,6,7)),
+        (False, (0,6,1,2)),
+        (True , (0,6,1,4)),
+        (False, (5,6,1,4)),
     )
 
     # o = marching_tetra_tri(positions, vals, iso, 0,2,3,7)
     # print(f' - positions {positions.min(0).values} -> {positions.max(0).values}')
     # print(' - using grid scale', 1/gridScale)
     out = torch.zeros((N,len(groups),2,3,3), device=D)
-    for i,grp in enumerate(groups):
-        out[:, i] = marching_tetra_tri(positions, vals, gridScale, iso, *grp)
+    for i,(swap,grp) in enumerate(groups):
+        out[:, i] = marching_tetra_tri(positions, vals, gridScale, iso, *grp, swap=swap)
 
     # Array of triangle vertex components.
     out1 = out.view(-1,9)
     N0 = out1.size(0)
+    # Remove invalid tris (should have 3x <-1,-,1-,1>)
     out1 = out1[(out1!=-1).all(1)]
     N1 = out1.size(0)
     out1 = out1.reshape(-1,3,3)
     # print(out1)
     print(f' - removed invalid tris: {N0} -> {N1}')
-
-    # TODO: Use sparse tensor again to reduce #verts (a lot are duplicates) and keep track of tri indices.
 
     out2 = out
     # out2 = out2.mul_(scale)
@@ -227,10 +273,16 @@ def marching_tetra(positions, vals, iso=0, gridScale=1):
     print('len verts', len(positions))
 
 
+    normals = compute_normals(positions, inds)
+    # normals = positions / np.linalg.norm(positions,axis=1,keepdims=True)
+
     colors = np.ones((positions.shape[0],4), dtype=np.float32)
-    colors[:,0] = positions[:,0]
-    # normals = compute_normals(positions, inds)
-    normals = positions / np.linalg.norm(positions,axis=1,keepdims=True)
+    # colors[:,0] = positions[:,0].abs() * 4
+    # colors[:,1] = positions[:,1].abs() * 4
+    # colors[:,2] = positions[:,2].abs() * 4
+    colors[:,0] = normals[:,0].abs() * 2
+    colors[:,1] = normals[:,1].abs() * 2
+    colors[:,2] = normals[:,2].abs() * 2
 
     from render import GridRenderer, glDisable, GL_CULL_FACE, glEnable
     r = GridRenderer((1024,)*2)
@@ -245,8 +297,8 @@ def marching_tetra(positions, vals, iso=0, gridScale=1):
             # posRange=(0,3),
             # normalRange=(3,6),
             )
-    glDisable(GL_CULL_FACE)
-    # glEnable(GL_CULL_FACE)
+    # glDisable(GL_CULL_FACE)
+    glEnable(GL_CULL_FACE)
 
     while True:
         r.startFrame()
@@ -260,7 +312,7 @@ def test_marching_tetra():
     coords = torch.cartesian_prod(*(torch.arange(SZ),)*3)
     # vals = coords[:, 0] / coords[:,0].max()
     # vals = coords[:, 0].float() / coords[:,0].max() #- .5 # Wall
-    vals = -((coords - SZ/2).float().norm(dim=1) - 10) # Sphere
+    vals = -(((coords - SZ/2).float() + coords.cos()).norm(dim=1) - 10) # Sphere
     # vals = (coords - (SZ+.5)/2).float().norm(dim=1) - .5
 
     st0 = torch.sparse_coo_tensor(coords.t(),vals, size=(SZ,)*3).coalesce()
