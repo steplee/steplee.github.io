5c98cd5e2d722a7307f94562d31096109ae80d5b
Stephen Lee <stephenl7797@gmail.com>
2023-04-14 17:21:46 -0400

    poisson sr work

--- extraPages/poisson/algo.py
+++ extraPages/poisson/algo.py
@@ -127,22 +127,166 @@ def get_stencil(iteration=2, kind='regular', size=-1, normalize='l1'):
 
     return img[0,0]
 
+def forward_into_grid(x, D, pad):
+    assert x.ndim == 2 and x.size(1) == 3
 
-# Sparse 3d convolutions.
-def form_system(pts, nrmls):
+    size = (1<<D)
+    scale = ((1<<D) - 2*pad) / (x.max() - x.min())
+    off = -x.min(0).values + pad/scale
+
+    x = x + off.view(1,3)
+    x.mul_(scale)
+
+    print(x.min())
+    print(x.max(), 1<<D)
+
+    assert (x>=0).all()
+    assert (x<=size).all()
+    return x, off, scale, size
+
+def backward_from_grid(x, off, scale):
+    x = x / scale
+    x.add_(off)
+    return x
+
+
+# Sparse 3d convolution.
+def form_system(pts0, nrmls0, D=8):
+    dev = pts0.device
+
+    # print(f' - stencil {stencil.shape}')
+    print(f' - pts {pts.shape}')
+    print(f' - nrmls {nrmls.shape}')
+
+    # Map to fixed resolution grid, then de-duplicate entries.
+    coo1, off, scale, size = forward_into_grid(pts0, D=D, pad=9)
+    st1 = torch.sparse_coo_tensor(coo1.long().t(), nrmls0, size=(size,size,size,3)).coalesce()
+    # st1._values().div_(st1._values().norm(dim=1,keepdim=True))
+    coo2, nrmls2 = st1.indices(), F.normalize(st1.values(), dim=1)
+
     stencil = get_stencil()
-    S = stencil.size(0)
+    stencil_st = stencil.to_sparse().coalesce() # A convenient way to iterate over indices/values
+    iter_stencil = lambda: zip(stencil_st.indices().t(), stencil_st.values())
 
-    # Iterate over the stencil, replicate
-    stencil = stencil.to_sparse().coalesce()
-    for D,W in zip(stencil.indices().t(), stencil.values()):
-        print(D,W)
+    # -----------------------------------------------------------------------------------------------------------
+    # Compute vector field V
+    #
+    Vc,Vv = torch.empty((3,0),dtype=torch.long,device=dev), torch.empty((0,3),dtype=torch.float32,device=dev)
+
+    for D,W in iter_stencil():
+        Vc = torch.cat((Vc, coo2 - D.view(3,1)), 1)
+        Vv = torch.cat((Vv, nrmls2), 0)
+
+    V = torch.sparse_coo_tensor(Vc,Vv, size=(size,size,size,3)).coalesce()
+    print(f' - V sizes (uncoalesced {Vc.size(1)}) (final nnz {V._nnz()}={V.indices().size(1)})')
+
+    # -----------------------------------------------------------------------------------------------------------
+    # Compute target `b` vector, whose components are the inner product of the basis functions Fo
+    # with the divergence of V
+    #
+    # The simplest way to do this would be to compute `div[V]` then convolve with Fo.
+    # That requires as much ram as V. However V is much larger than {Fo}.
+    # So (perhaps?) another way to compute it is by considering Fo as the main thing.
+    #
+    # Here is the first one.
+
+    # Compute div[V]
+    # NOTE: This relies on linearity. It computes the divergnce by a sparse 3d conv (not memory efficient)
+    # NOTE: There are some tricks I see to improve efficiency here (using sparse tensor shifts by multiplying
+    #                                                               that could reduce mem consumption)
+    # WARNING: This is a convolution of a convolution. It requires even more padding of the input grid to
+    #          prevent invalid indices. Instead of that, I'll mask out invalid entries.
+    #          This also increases memory usage upto 6x!
+    #          But we only need these data at certain points (those indices occupied by V)
+    #          You could form a sparse tensor at each iter and multiply to keep only needed entries OR
+    #          use the other method of computing that does not have this issue...
+    # FIXME: going to need the shift/multiply based approach, this hits the worse case 6x (of the already 125x V).
+    #        that is RAM usage upto 750x initial point set...
+    # NOTE: There may even be a better way that what I had in mind by casting as sparse matrix multiply...
+    Vc2 = V.indices()
+    Vv2 = V.values()
+    if 0:
+        # Not sure if works, but next branch does
+        divVc,divVv = torch.empty((3,0),dtype=torch.long,device=dev), torch.empty((0),dtype=torch.float32,device=dev)
+        def collapse_divV():
+            nonlocal divVc, divVv
+            mask  = (divVc>=0).all(0) & (divVc<size).all(0)
+            divVc = divVc[:,mask]
+            divVv = divVv[mask]
+            divV  = torch.sparse_coo_tensor(divVc,divVv, size=(size,size,size,1)).coalesce()
+            divVc,divVv = divV.indices(), divV.values()
+            return divV
+        for d in range(3):
+            d = torch.LongTensor((d==0,d==1,d==2)).view(3,1)
+            divVc = torch.cat((divVc, Vc2 - D.view(3,1)), 1)
+            divVv = torch.cat((divVv, -Vv2.sum(1,keepdim=True)), 0)
+            divVc = torch.cat((divVc, Vc2 + D.view(3,1)), 1)
+            divVv = torch.cat((divVv, Vv2.sum(1,keepdim=True)), 0)
+            collapse_divV() # This is optional. Trades less speed for less RAM usage
+        divV = collapse_divV()
+        print(f' - divV sizes (uncoalesced {divVc.size(1)}) (final nnz {divV._nnz()})')
+        # Sub-select divV to have *EXACT* same indices as V
+        divV = divV * torch.sparse_coo_tensor(V.indices(), torch.ones_like(V.values()[:,0:1]), size=(*V.size()[:3],1))
+        print(f' - divV subselected nnz {divV._nnz()}')
+    else:
+        accessor = torch.sparse_coo_tensor(Vc2, torch.ones (Vc2.size(1),device=dev), size=V.size()[:3]).coalesce()
+        divV     = torch.sparse_coo_tensor(Vc2, torch.zeros(Vc2.size(1),device=dev), size=V.size()[:3]).coalesce()
+        for d in range(3):
+            dd = torch.LongTensor((d==0,d==1,d==2)).view(3,1).to(dev)
+            if 0:
+                left  = (torch.sparse_coo_tensor(Vc2 + dd.view(3,1), Vv2[:,d], size=V.size()[:3]).coalesce() * accessor).coalesce()
+                right = (torch.sparse_coo_tensor(Vc2 - dd.view(3,1), Vv2[:,d], size=V.size()[:3]).coalesce() * accessor).coalesce()
+            else:
+                l = Vc2 + dd.view(3,1)
+                r = Vc2 - dd.view(3,1)
+                lv = Vv2[(l>=0).all(0) & (l<size).all(0)][:,d]
+                rv = Vv2[(r>=0).all(0) & (r<size).all(0)][:,d]
+                # lv = Vv2[:,d]
+                # rv = Vv2[:,d]
+                left  = (torch.sparse_coo_tensor(l, lv, size=V.size()[:3]).coalesce() * accessor).coalesce()
+                right = (torch.sparse_coo_tensor(r, rv, size=V.size()[:3]).coalesce() * accessor).coalesce()
+                divV += (right - left).coalesce()
+        divV = divV.coalesce()
+        # Leave the zeros.
+        # mask = divV.values() != 0
+        # divV = torch.sparse_coo_tensor(divV.indices()[:,mask],divV.values()[mask])
+        print('V:\n',V)
+        print('divV:\n',divV)
+        print(' -     V  shape', V._nnz())
+        print(' - div[V] shape', divV._nnz())
+
+
+    # Compute vector v, with each coordinate v_o = <div[V] , Fo>
+
+    # It is now necessary to use actual sparse *matrices* instead of using st's as arrays.
+    # div[V] will be a matrix. We'll select only the entries from the tensor that correspond to nodes {o}.
+    # Fo will be a sparse matrix.
+
+    # In this section F will need to be a matrix that maps:
+    #      from the size of div[V] (same as size of V, upto 125x the size of octree)
+    #      to the size of O (the octree)
+
+    # vc,vv = torch.empty((3,0),dtype=torch.long,device=dev), torch.empty((0),dtype=torch.float32,device=dev)
+    # for D,W in iter_stencil():
+        # Vc = torch.cat((Vc, coo2 - D.view(3,1)), 1)
+        # Vv = torch.cat((Vv, nrmls2), 0)
+
+    # NOTE: to compute the v vector:
+    #     1) Form sparse matrix Fv which maps R^|V| -> R^|O|
+    #          a) form Vind. Has same indices as V, but values as arange(len(V))
+    #          b) form Oones, indices same as O, but values as ones.
+    #          c) do 3d sparse conv, shifting with Vind, weighing with W, and masking with Oones
+    #              i) Add columns/rows of final matrix each iter.
+    #             ii) The matrix is exactly |O| rows and has |V| columns, but only upto 125 entries per row.
 
 
 
 
 
+
+torch.manual_seed(0)
 pts   = torch.randn(512, 3).cuda()
-nrmls = torch.randn(512, 3).cuda()
+pts   = pts / pts.norm(dim=1,keepdim=True)
+nrmls = pts
 form_system(pts, nrmls)
 viz_box()
---
+++ extraPages/poisson/solvers.py
@@ -0,0 +1,212 @@
+import torch, torch.nn.functional as F
+
+'''
+Mostly just some linear algebra and calculus review.
+Also some from https://www.cs.cmu.edu/~quake-papers/painless-conjugate-gradient.pdf
+'''
+
+
+# Generates a rotation matrix (det=+1 or -1, but evalues may be imaginary and/or negative)
+# NOTE: Input is expected to be a row-major set of vectors.
+#       Output is an orthogonal set of column vectors (also row ortho...).
+def gram_schmidt(vs):
+    N = len(vs)
+    B = torch.zeros((N,N))
+    for i in range(N):
+        u = vs[i]
+        for j in range(i):
+            u -= B[:,j] * (B[:,j] @ u) / (B[:,j]@B[:,j])
+        B[:,i] = u
+    return B / B.norm(dim=0,keepdim=True)
+
+# Section 7.2.
+# Like above, but vectors are A-conjugate, not orthogonal w.r.t standard basis.
+def gram_schmidt_conjugate(vs, A):
+    N = len(vs)
+    B = torch.zeros((N,N))
+    for i in range(N):
+        u = vs[i]
+        for j in range(i):
+            u -= B[:,j] * (u @A@ B[:,j]) / (B[:,j]@A@B[:,j])
+        B[:,i] = u
+    return B / B.norm(dim=0,keepdim=True)
+
+# Test gram schmidt, work on generating random symmetric-PD matrix
+if 0:
+    A = torch.FloatTensor((
+        1,0,0,
+        0,2,2,
+        1,2,1)).reshape(3,3)
+    A = torch.randn(4,4)
+    # A = (A@A.mT)
+    # A = (A+A.mT)
+    print(f' - A:\n{A}')
+    B = gram_schmidt(A)
+    evalues = torch.linspace(.1,5,4) ** 2
+    B = B @ evalues.diag() @ B.mT
+    print(f' - B:\n{B}')
+    print(f' - B@B.T:\n{B@B.mT}')
+    print(f' - B.T@B:\n{B.mT@B}')
+    print(f' - Eig{{B}}:\n{torch.linalg.eig(B)}')
+    print(f' - Det{{B}}: {torch.linalg.det(B)}')
+    max_div_min = lambda t: (t.max() / t.min()).item()
+    print(f' - Condition{{B}} {max_div_min(torch.linalg.eig(B).eigenvalues.real)}')
+    exit()
+
+# Look at conjugate GS
+if 0:
+    A = torch.FloatTensor((
+        1,0,1,
+        0,5,0,
+        1,0,4)).reshape(3,3) # In a least-squares setting, the matrix will be symmetric-PD
+    # A = torch.eye(3) # This is really the simplest check.
+
+    V = torch.FloatTensor((
+        1,0,0,
+        0,2,2,
+        1,2,1)).reshape(3,3)
+
+    B = gram_schmidt_conjugate(V,A)
+    # evalues = torch.linspace(.1,5,3) ** 2
+    # B = B @ evalues.diag() @ B.mT
+    print(f' - B:\n{B}')
+    print(f' - B@B.T:\n{B@B.mT}')
+    print(f' - B.T@B:\n{B.mT@B}')
+    print(f' - Eig{{B}}:\n{torch.linalg.eig(B)}')
+    print(f' - Det{{B}}: {torch.linalg.det(B)}')
+    max_div_min = lambda t: (t.max() / t.min()).item()
+    print(f' - Condition{{B}} {max_div_min(torch.linalg.eig(B).eigenvalues.real)}')
+    # WARNING: Orthonormality applies to both rows and columns.
+    # WARNING: But conjugacy does not. The GS process's outputs are only necessarily column-wise conjugate!
+    print(f' - A-conjugacy:\n{B.mT@A@B}')
+    exit()
+
+
+def get_problem_1():
+
+    if 0:
+        A = torch.FloatTensor((150,21,21,3.)).view(2,2)
+        b = torch.FloatTensor((2,-8.))
+    else:
+        # A random but spectrum-controlled symmetric-PD matrix.
+        torch.manual_seed(0)
+        N = 4
+        A = torch.randn(N,N)
+        A = (A@A.mT)
+        A = gram_schmidt(A)
+        evalues = torch.linspace(.1, 5, N) ** 2
+        A = A @ evalues.diag() @ A.mT
+        b = torch.randn(N)
+    x = torch.linalg.solve(A,b)
+
+    print('')
+    print(f' - A\n{A}')
+    print(f' - Eig{{A}} {torch.linalg.eig(A).eigenvalues.real}')
+    max_div_min = lambda t: (t.max() / t.min()).item()
+    print(f' - Condition{{A}} {max_div_min(torch.linalg.eig(A).eigenvalues.real)}')
+    print(f' - Ground-Truth Solved x {x}')
+    print('')
+
+    return A, b, x
+
+
+def run_generic(A, b, x, T, x0, solve_func, **kw):
+    def getError(xi): return (x-xi).norm()
+    def getRes(xi): return b-A@xi
+
+    print(f'error0 {getError(x0)}')
+    print(f'res0   {getRes(x0)}')
+
+    xi = solve_func(A, b, x0, T, **kw)
+
+    print(f'errorT {getError(xi)}')
+    print(f'resT   {getRes(xi)}')
+
+def solve_steepest_descent(A, b, x0, T):
+    xi = x0
+    for i in range(T):
+        r = b - A@xi
+        a = (r@r) / (r@A@r + 1e-9)
+        xi1 = xi + a*r
+        xi = xi1
+        if r.norm() < 1e-7: break
+    return xi
+
+def solve_jacobi(A, b, x0, T):
+    # Split Matrix
+    D = A * torch.eye(A.size(0))
+    E = A - D
+
+    # Form iteration matrices
+    Dinv = (1./torch.diag(D)).diag()
+    B = -Dinv @ E
+    z = Dinv @ b
+
+    print(f'Spectral Radius of \'B\' (max evalue, smaller better) ---> {torch.linalg.eig(B).eigenvalues.abs().max()}')
+
+    xi = x0
+    for i in range(T):
+        xi = B @ xi + z
+    return xi
+
+# Only allows jacobi (diagonal) preconditioning
+def solve_cg(A, b, x0, T, preconditioned=False):
+    x = x0
+    r = b - A @ x
+    Minv = 1./A.diag() if preconditioned else None
+    # Minv = torch.eye(x.size(0)).diag()
+    d = r if not preconditioned else Minv * r
+
+    ds = []
+    rs = []
+
+    T = min(T, len(b)*2)
+
+    # FIXME: Shewchuk avoids r1 by storing the inner product for the next iter (he calls delta_new and delta_old)
+    for i in range(T):
+        ds.append(d); rs.append(r)
+
+        a = ((r@r) if not preconditioned else (r@(Minv*r))) / (d@A@d)
+        x = x + a * d
+        r1 = r - a * (A@d)
+        if not preconditioned:
+            beta = (r1 @ r1) / (r@r)
+            d = r1 + beta * d
+        else:
+            s = Minv*r1
+            beta = (r1 @ s) / (r@(Minv*r))
+            d = s + beta * d
+
+        r = r1
+        if r.norm() < 1e-7: break
+
+    ds = torch.stack(ds[:8],0)
+    rs = torch.stack(rs[:8],0)
+    print(f' - first 8 ds A-conjugacy:\n{ds@A@ds.mT}') # The point is to have zeros on the off-diagonals here
+    # print(f' - rs A-conjugacy:\n{rs@A@rs.mT}')
+
+    return x
+
+
+
+
+if 1:
+    torch.set_printoptions(linewidth=120, sci_mode=False)
+    A, b, x = get_problem_1()
+
+    N = b.size(0)
+    x0 = torch.full((N,), -2.)
+    T = 100
+
+    print('\n**********************************')
+    print(' Running Steepest Descent')
+    run_generic(A, b, x, T, x0, solve_func=solve_steepest_descent)
+    print('\n**********************************')
+    print(' Running Jacobi')
+    run_generic(A, b, x, T, x0, solve_func=solve_jacobi)
+    print('\n**********************************')
+    print(' Running CG')
+    run_generic(A, b, x, T, x0, solve_func=solve_cg)
+    print('\n**********************************')
+    print(' Running PCG')
+    run_generic(A, b, x, T, x0, solve_func=solve_cg, preconditioned=True)
